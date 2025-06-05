f6e49720df9bdfc556d09852db293da7e77df02a
Jay1741 <j_darji233248@fanshaweonline.ca>
2025-06-05 18:23:23 -0400

    Add files via upload

---
+++ week-5-lecture-s2/scripts/app.js
@@ -0,0 +1,31 @@
+console.log('JS loaded!');
+// js comment here :)
+
+let programName = 'Embedded Systems';
+let year = 2025;
+console.log(programName);
+console.log('2025');
+console.log(year);
+programName = 'video editing';
+console.log(programName);
+
+const collegeName = 'Fanshawe';
+console.log(collegeName);
+
+// collegeName = 'TriOS';
+// console.log(collegeName);
+
+function myCustomFunctionNameHere(number, secondValue) {
+    return number + 5 + secondValue;
+}
+
+// call the function
+console.log(myCustomFunctionNameHere(15, 'liam'));
+
+let firstName = 'Liam';
+let lastName = 'Stewart';
+
+let customVariableHere = `${firstName} ${lastName} asuidhiasd${lastName}iasd ihasidhasd ${lastName}ihasidhasd iojasdasd`;
+
+console.log(customVariableHere);
+
